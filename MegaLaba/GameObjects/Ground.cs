8c583636d43776d299429ce84890f608ced6b9b6
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:57:30 +0500

    Create Ground.cs

---
+++ MegaLaba/GameObjects/Ground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace MegaLaba
+{
+    /// <summary>
+    /// Класс земли
+    /// </summary>
+    class Ground : GameObject
+    {
+        public Ground(GameObjectLocation objectPlace)
+        {
+            Figure = GameSettings.Ground;
+            GameObjectLocation = objectPlace;
+        }
+
+        public static List<GameObject> GetGround()
+        {
+            List<GameObject> ground = new List<GameObject>();
+
+            int startX = GameSettings.GroundStartX;
+            int startY = GameSettings.GroundStartY;
+
+            for (int y = 0; y < GameSettings.NumberOfGroundRows; y++) //по строкам
+            {
+                for (int x = 0; x < GameSettings.NumberOfGround; x++) //по эл-м
+                {
+                    GameObjectLocation objectPlace = new GameObjectLocation() { X = startX + x, Y = startY + y };
+                    GameObject groundObj = new Ground(objectPlace);
+                    ground.Add(groundObj);
+                }
+            }
+            return ground;
+        }
+    }
+}
