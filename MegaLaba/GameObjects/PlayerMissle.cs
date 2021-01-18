e7c70fdfd7bcc679d591d7bbf936421a7ea959da
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:57:38 +0500

    Create PlayerMissle.cs

---
+++ MegaLaba/GameObjects/PlayerMissle.cs
@@ -0,0 +1,15 @@
+﻿namespace MegaLaba
+{
+    /// <summary>
+    /// Класс инициализации Снарядов пользовательского корабля
+    /// </summary>
+    class PlayerMissle : GameObject
+    {
+        public PlayerMissle(GameObjectLocation objectPlace)
+        {
+            GameObjectLocation misslePlace = new GameObjectLocation() { X = objectPlace.X, Y = objectPlace.Y - 1 };
+            Figure = GameSettings.Missle;
+            GameObjectLocation = misslePlace;
+        }
+    }
+}
