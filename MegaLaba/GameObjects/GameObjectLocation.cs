cfdd9db25bd517ebc3bd55fd6acad80f322e0639
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:58:14 +0500

    Create GameObjectLocation.cs

---
+++ MegaLaba/GameObjects/GameObjectLocation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MegaLaba
+{
+    /// <summary>
+    /// Класс координат 
+    /// </summary>
+    class GameObjectLocation
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public override bool Equals(object place)
+        {
+            GameObjectLocation location = place as GameObjectLocation;
+            if (location != null && X == location.X && Y == location.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
