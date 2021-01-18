41b05c0bc1c188b96f2f537b41370475d565c6ef
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:58:07 +0500

    Create Scene.cs

---
+++ MegaLaba/Scene.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MegaLaba
+{
+    /// <summary>
+    /// Класс сцены
+    /// </summary>
+    class Scene
+    {
+        /// <summary>
+        /// Коллекция объектов кораблей пришельцев (рой)
+        /// </summary>
+        public List<GameObject> invaders;
+
+        /// <summary>
+        /// Коолекция объектов земли
+        /// </summary>
+        public List<GameObject> ground;
+
+        /// <summary>
+        /// Пользовательский корабль
+        /// </summary>
+        public object player;
+
+        /// <summary>
+        /// Коллекция объектов ракет
+        /// </summary>
+        public List<GameObject> playerMissle;
+        public List<GameObject> invaderMissle;
+
+        public static Type PlayerT = Type.GetTypeFromProgID("ComObjects.Player");
+
+        public Scene() //инициализация коллекции
+        {
+            invaders = Invader.GetInvaders();
+            ground = Ground.GetGround();
+            
+            player = Activator.CreateInstance(PlayerT, GameSettings.PlayerStartX, GameSettings.PlayerStartY, GameSettings.Player);
+            playerMissle = new List<GameObject>();
+            invaderMissle = new List<GameObject>(); 
+        }
+    }
+}
