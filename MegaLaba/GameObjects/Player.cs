012701f7972b10d5349958bfe84822ce419ac79d
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:57:49 +0500

    Create Player.cs

---
+++ MegaLaba/GameObjects/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace MegaLaba
+{
+    /// <summary>
+    /// Класс инициализации Пользовательского корабля
+    /// </summary>
+    //class Player : GameObject
+    //{
+    //    public Player()
+    //    {
+    //        GameObjectLocation location = new GameObjectLocation()
+    //        {
+    //            X = GameSettings.PlayerStartX,
+    //            Y = GameSettings.PlayerStartY
+    //        };
+    //        Figure = GameSettings.Player;
+    //        GameObjectLocation = location;
+    //    }
+    //}
+}
