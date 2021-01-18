b058203284930062e8c332890d8da06edabacb84
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:58:16 +0500

    Create GameObject.cs

---
+++ MegaLaba/GameObjects/GameObject.cs
@@ -0,0 +1,12 @@
+﻿namespace MegaLaba
+{
+	/// <summary>
+	/// Базовый класс объектов
+	/// </summary>
+	abstract class GameObject
+	{
+        public GameObjectLocation GameObjectLocation { get; set; }
+
+        public char Figure { get; set; }
+    }
+}
