ed350e1c66951cb6eafbee603818f64e9e21b896
Alexey Ponomarev <stud0000126422@study.utmn.ru>
2021-01-18 13:57:33 +0500

    Create InvaderMissle.cs

---
+++ MegaLaba/GameObjects/InvaderMissle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaLaba
+{
+    class InvaderMissle : GameObject
+    {
+        public InvaderMissle(GameObjectLocation objectPlace)
+        {
+            GameObjectLocation misslePlace = new GameObjectLocation() { X = objectPlace.X, Y = objectPlace.Y + 1 };
+            Figure = GameSettings.Missle;
+            GameObjectLocation = misslePlace;
+        }
+    }
+}
